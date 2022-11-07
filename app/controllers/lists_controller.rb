d6b68a7c2b8ebc36879ef864f4389f4e7c3e29b1
mgmdesu <115972724+mgmdesu@users.noreply.github.com>
2022-11-07 07:03:01 +0000

    [finish]3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   def new
-    @list=List.new
-  end
+    @list = List.new
+  end 
   
   def create
     @list = List.new(list_params)
@@ -13,25 +13,25 @@ class ListsController < ApplicationController
   end 
 
   def index
-    @lists=List.all
+    @lists = List.all
   end
 
   def show
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
-    @list=List.find(params[:id])
+    @list = List.find(params[:id])
   end
   
   def update
-    list=List.find(params[:id])
+    list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)
   end
   
   def destroy
-    list=List.find(params[:id])
+    list = List.find(params[:id])
     list.destroy
     redirect_to '/lists'
   end 
